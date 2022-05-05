a979ef9370ad5b9155db65d068dc73438b941c9d
Mateusz Skwierczyński <mskw@Mateuszs-MBP.home>
2022-05-05 17:25:22 +0200

    feat: setup validation

--- src/components/validation/validation.component.tsx
+++ src/components/validation/validation.component.tsx
@@ -8,8 +8,56 @@ import {
   InputLeftElement,
   InputRightElement,
 } from "@chakra-ui/react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export function ValidationComponent() {
+  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
+  const [inProgress, setInProgress] = useState(false);
+  const [blockTime, setBlockTime] = useState(14);
+
+  const clearInt = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+  };
+
+  useEffect(() => {
+    () => {
+      clearInt();
+    };
+  }, []);
+
+  const handleValidation = useCallback(() => {
+    if (intervalRef.current) {
+      setInProgress(false);
+      clearInt();
+    } else {
+      setInProgress(true);
+      intervalRef.current = setInterval(handleTick, 1000);
+    }
+  }, []);
+
+  const validate = useCallback(() => {
+    // TODO: validate
+    console.log("validate");
+    setTimeout(() => {
+      setBlockTime(14);
+      handleValidation();
+    }, 5000);
+  }, [handleValidation]);
+
+  useEffect(() => {
+    if (blockTime === 0) {
+      clearInt();
+      validate();
+    }
+  }, [blockTime, validate]);
+
+  const handleTick = () => {
+    setBlockTime((v) => v - 1);
+  };
+
   return (
     <Flex pt="4" direction="column">
       <Text pb="2">Block time [s]</Text>
@@ -17,10 +65,17 @@ export function ValidationComponent() {
         <InputLeftElement pointerEvents="none">
           <TimeIcon color="gray.300" />
         </InputLeftElement>
-        <Input placeholder="Block time" value={14} type="number" />
+        <Input
+          placeholder="Block time"
+          _disabled={{ opacity: 1 }}
+          value={blockTime}
+          disabled={inProgress}
+          onChange={(e) => setBlockTime(parseInt(e.target.value))}
+          type="number"
+        />
         <InputRightElement width="4.5rem">
-          <Button h="1.75rem" size="sm">
-            Start
+          <Button h="1.75rem" size="sm" onClick={handleValidation}>
+            {inProgress ? "Stop" : "Start"}
           </Button>
         </InputRightElement>
       </InputGroup>
