3c25642cdbfa9ea5285a569875a8e0053ae09166
Mateusz Skwierczyński <mskw@Mateuszs-MacBook-Pro.local>
2022-05-07 21:52:02 +0200

    feat: add link to github

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -3,7 +3,12 @@ import { AiFillGithub } from "react-icons/ai";
 export function FooterComponent() {
   return (
     <Flex as="footer" height="28" alignItems="center" justifyContent="center">
-      <Icon as={AiFillGithub} w={10} h={10} />
+      <a
+        href="https://github.com/mskw23/blockchain-demo"
+        target="_blank"
+        rel="noreferrer">
+        <Icon as={AiFillGithub} w={10} h={10} />
+      </a>
     </Flex>
   );
 }
